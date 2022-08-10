ec4b70944a0e65f6cd77746833c06fdeb3dc5a7f
Osborn Essien <106802309+Osbee205@users.noreply.github.com>
2022-08-10 01:15:26 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -97,7 +97,6 @@ int print_percent(va_list types, char buffer[],
 	return (write(1, "%%", 1));
 }
 
-
 /************************* PRINT INT *************************/
 /**
  * print_int - Print int
@@ -166,4 +165,25 @@ int print_binary(va_list types, char buffer[],
 	UNUSED(precision);
 	UNUSED(size);
 
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
 
